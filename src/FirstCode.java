c9c4d2243ea668e35f37cbfb661484a8831ba82f
rashad660 <54605625+rashad660@users.noreply.github.com>
2019-09-20 23:05:00 -0400

    Java

---
+++ src/FirstCode.java
@@ -0,0 +1,10 @@
+public class FirstCode {
+
+
+    public static void main(String args[] )
+    {
+        System.out.println("Hello World");
+        System.out.println("Salam Dunya");
+        System.out.println("Merhaba Dunya");
+    }
+}
