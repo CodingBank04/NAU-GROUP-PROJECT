e2bd07d8704c67255ea2b26258e2364e27f57a7b
MCengiz16 <48599879+MCengiz16@users.noreply.github.com>
2019-11-14 20:30:39 -0600

    Final

---
+++ src/main/java/Pages/HomePage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+import base.Base;
+
+public class HomePage extends Base {
+    public static void main(String[] args) {
+        driver.get (url);
+    }
+}
---
+++ src/main/java/base/Base.java
@@ -0,0 +1,36 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.PublicKey;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Set;
+
+public class Base {
+
+    public static WebDriver driver=driver ();
+
+    public static String url= "http://qatest2119.na.edu/";
+
+    private static WebDriver driver(){
+        WebDriverManager.chromedriver().setup();
+        return  new ChromeDriver ();
+    }
+
+
+
+
+
+
+}
